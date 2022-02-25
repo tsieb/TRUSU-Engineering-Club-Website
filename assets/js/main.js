e9c34fad6f94767652bd03643f28cda98adcb45e
tsieb <58095853+tsieb@users.noreply.github.com>
2022-02-25 03:23:27 -0800

    new site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,92 +1,185 @@
-jQuery(document).ready(function () {
+/*
+	Phantom by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
-    jQuery(".play-1, .play-2").yu2fvl();
+(function($) {
 
-    jQuery(".owl-carousel4").owlCarousel(
-        {
-            loop: true,
-            center: true,
-            margin: 20,
-            responsiveClass: true,
-            nav: true,
-            responsive: {
-                0: {
-                    items: 2,
+	var	$window = $(window),
+		$body = $('body');
 
-                },
-                600: {
-                    items: 3,
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
 
-                },
-                1000: {
-                    items: 5,
-                    nav: true,
-                    loop: true
-                }
-            }
-        }
-    );
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
-    jQuery(".owl-carousel5").owlCarousel(
-        {
-            loop: true,
-            center: false,
-            margin: 10,
-            responsiveClass: true,
-            nav: false,
-            responsive: {
-                0: {
-                    items: 2,
+	// Touch?
+		if (browser.mobile)
+			$body.addClass('is-touch');
 
-                },
-                600: {
-                    items: 3,
+	// Forms.
+		var $form = $('form');
 
-                },
-                1000: {
-                    items: 6,
-                    nav: false,
-                    loop: true
-                }
-            }
-        }
-    );
+		// Auto-resizing textareas.
+			$form.find('textarea').each(function() {
 
-});
+				var $this = $(this),
+					$wrapper = $('<div class="textarea-wrapper"></div>'),
+					$submits = $this.find('input[type="submit"]');
 
-function myFunction(x) {
-    x.classList.toggle("change");
-}
+				$this
+					.wrap($wrapper)
+					.attr('rows', 1)
+					.css('overflow', 'hidden')
+					.css('resize', 'none')
+					.on('keydown', function(event) {
 
+						if (event.keyCode == 13
+						&&	event.ctrlKey) {
 
+							event.preventDefault();
+							event.stopPropagation();
 
-jQuery(".link-img").click(function () {
-    var link = jQuery(this).attr("data-link");
-    //alert(link);
-    jQuery("#screen").attr("src", link)
-});
+							$(this).blur();
 
+						}
 
+					})
+					.on('blur focus', function() {
+						$this.val($.trim($this.val()));
+					})
+					.on('input blur focus --init', function() {
 
-var count = 0;
-jQuery("#toggle-search").click(function () {
-    count++;
-    //even odd click detect 
-    var isEven = function (someNumber) {
-        return (someNumber % 2 === 0) ? true : false;
-    };
-    // on odd clicks do this
-    if (isEven(count) === false) {
-        //jQuery("#nav-search").css({"display":"block"});
-        jQuery("#nav-search").slideDown();
-        jQuery("#toggle-search").attr("src", "assets/images/close.png");
+						$wrapper
+							.css('height', $this.height());
 
-    }
-    // on even clicks do this
-    else if (isEven(count) === true) {
-        //jQuery("#nav-search").css({"display":"none"});
-        jQuery("#nav-search").slideUp();
+						$this
+							.css('height', 'auto')
+							.css('height', $this.prop('scrollHeight') + 'px');
 
-        jQuery("#toggle-search").attr("src", "assets/images/search-icon.png");
-    }
-});
+					})
+					.on('keyup', function(event) {
+
+						if (event.keyCode == 9)
+							$this
+								.select();
+
+					})
+					.triggerHandler('--init');
+
+				// Fix.
+					if (browser.name == 'ie'
+					||	browser.mobile)
+						$this
+							.css('max-height', '10em')
+							.css('overflow-y', 'auto');
+
+			});
+
+	// Menu.
+		var $menu = $('#menu');
+
+		$menu.wrapInner('<div class="inner"></div>');
+
+		$menu._locked = false;
+
+		$menu._lock = function() {
+
+			if ($menu._locked)
+				return false;
+
+			$menu._locked = true;
+
+			window.setTimeout(function() {
+				$menu._locked = false;
+			}, 350);
+
+			return true;
+
+		};
+
+		$menu._show = function() {
+
+			if ($menu._lock())
+				$body.addClass('is-menu-visible');
+
+		};
+
+		$menu._hide = function() {
+
+			if ($menu._lock())
+				$body.removeClass('is-menu-visible');
+
+		};
+
+		$menu._toggle = function() {
+
+			if ($menu._lock())
+				$body.toggleClass('is-menu-visible');
+
+		};
+
+		$menu
+			.appendTo($body)
+			.on('click', function(event) {
+				event.stopPropagation();
+			})
+			.on('click', 'a', function(event) {
+
+				var href = $(this).attr('href');
+
+				event.preventDefault();
+				event.stopPropagation();
+
+				// Hide.
+					$menu._hide();
+
+				// Redirect.
+					if (href == '#menu')
+						return;
+
+					window.setTimeout(function() {
+						window.location.href = href;
+					}, 350);
+
+			})
+			.append('<a class="close" href="#menu">Close</a>');
+
+		$body
+			.on('click', 'a[href="#menu"]', function(event) {
+
+				event.stopPropagation();
+				event.preventDefault();
+
+				// Toggle.
+					$menu._toggle();
+
+			})
+			.on('click', function(event) {
+
+				// Hide.
+					$menu._hide();
+
+			})
+			.on('keydown', function(event) {
+
+				// Hide on escape.
+					if (event.keyCode == 27)
+						$menu._hide();
+
+			});
+
+})(jQuery);
